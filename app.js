13bf7deb833064028f721c32a6dc5b965d518e13
unknown <ludgerk@techlaunch.com>
2019-02-20 17:52:58 -0500

    changed port

--- app.js
+++ app.js
@@ -65,29 +65,29 @@ app.use(
 );
 
 
-// ===============================
+// // ===============================
 
-app.use((req, res, next) => {
-  const allowedOrigins = ['http://kludger.techlaunch.io:3004', 'http://142.93.115.171:3004'];
-  const origin = req.headers.origin;
-if (allowedOrigins.indexOf(origin) > -1) {
-  res.setHeader('Access-Control-Allow-Origin', origin);
-}
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// app.use((req, res, next) => {
+//   const allowedOrigins = ['http://kludger.techlaunch.io:80', 'http://142.93.115.171:80'];
+//   const origin = req.headers.origin;
+// if (allowedOrigins.indexOf(origin) > -1) {
+//   res.setHeader('Access-Control-Allow-Origin', origin);
+// }
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
-// ===============================
+// // ===============================
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -155,7 +155,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(result => {
-    app.listen(process.env.PORT || 3004);
+    app.listen(process.env.PORT || 80);
   })
   .catch(err => {
     console.log(err);
